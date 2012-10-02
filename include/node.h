dc785ac7965e576c4f576999d3529de60cd135f6
Neil Fulwiler <Neil_Fulwiler@brown.edu>
2012-10-01 20:21:39 -0400

    Pushing node fil

--- include/node.h
+++ include/node.h
@@ -1,10 +1,12 @@
 #ifndef __NODE_H__
 #define __NODE_H__
 
+#include "list.h"
+
 typedef struct node* node_t; 
 
-node_t node_init();
-void node_destroy(node_t*);
+node_t node_init(link_t* link);
+void node_destroy(node_t* node);
 
 void node_start();
 void node_stop();
