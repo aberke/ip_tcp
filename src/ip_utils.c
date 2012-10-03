29bb4ffc2b538f56459b4b54b106fc9f385a8b5f
Alexandra Berke <alexandra_berke@brown.edu>
2012-10-03 09:03:17 -0400

    having trouble getting makefile to see ip_sum

--- src/ip_utils.c
+++ src/ip_utils.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <netinet/ip.h>
 #include <inttypes.h>
 
 #include "ip_utils.h"
-#include "util/ipsum.h"
+#include "ipsum.h"
 
 #define RIP_DATA 200  
 #define TEST_DATA 0  
@@ -22,21 +26,20 @@ simply print them out in a useful way."*/
 
 //Param: buffer read in, number of bytes read in
 //Return value: bytes of data within packet on success, -1 on error
-int packet_data_size = ip_check_valid_packet(char* buffer, int bytes_read){
+int ip_check_valid_packet(char* buffer, int bytes_read){
 	//make sure packet long enough to be an ip packet
 	if(bytes_read < sizeof(struct ip)){
 		//packet not large enough
-		puts("received packet with length < sizeof(struct ip)");
+		printf("received packet with length < sizeof(struct ip)");
 		return -1;
 	}
-	u_int header_length;
-	u_short ip_len, ip_sum;
+	u_short checksum;
 	char header[sizeof(struct ip)];
 	memcpy(header, buffer, sizeof(struct ip));
 	struct ip *ip_header = (struct ip *)header;
 	//run checksum
-	ip_sum = ip_header->ip_sum;
-	if(ip_sum != ip_sum(header, sizeof(struct ip)){
+	checksum = ip_header->ip_sum;
+	if(checksum != ip_sum(header, sizeof(struct ip)){
 		puts("Packet ip_sum != actually checksum");
 		return -1;
 	}
@@ -47,30 +50,32 @@ int packet_data_size = ip_check_valid_packet(char* buffer, int bytes_read){
 		puts("bytes read in less than packet length");
 		return -1;
 	}
+	u_int header_length;
 	header_length = ip_header->ip_hl;
-	return ip_len - ip_hl;
+	int data_len = ip_len - header_length;
+	return data_len;
 }
 // returns destination address of packet
-uint32_t dest_addr = ip_get_dest_addr(char* buffer){
+uint32_t ip_get_dest_addr(char* buffer){
 	char header[sizeof(struct ip)];
 	memcpy(header, buffer, sizeof(struct ip));
 	struct ip *ip_header = (struct ip *)header;
 	
 	struct  in_addr dest_ip;
 	uint32_t d_addr;
-	ip_dest = ip_header->ip_dest;
-	d_addr = ip_dest.s_addr;
+	dest_ip = ip_header->ip_dst;
+	d_addr = dest_ip.s_addr;
 	return d_addr;
 }
 // int is type: RIP vs other  --return -1 if bad packet
 // fills packet_unwrapped with data within packet
-int type = ip_unwrap_packet(char* buffer, char* packet_unwrapped, int packet_data_size){
-	u_int header_length;
+int ip_unwrap_packet(char* buffer, char* packet_unwrapped, int packet_data_size){
+	u_int header_len;
 	u_char ip_p;           /* protocol */
 	char header[sizeof(struct ip)];
 	memcpy(header, buffer, sizeof(struct ip));
 	struct ip *ip_header = (struct ip *)header;
-	header_length = ip_header->ip_hl;
+	header_len = ip_header->ip_hl;
 	ip_p = ip_header->ip_p;
 	memcpy(packet_unwrapped, buffer+header_len, packet_data_size);
 	if(ip_p == RIP_DATA){
