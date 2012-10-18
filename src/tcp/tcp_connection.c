100d667a5463433b2feb1dbc709c233455700e3a
Alexandra Berke <alexandra_berke@brown.edu>
2012-10-18 18:00:28 -0400

    Right now working on structuring our project:  Laying out potential ground-work for architecture.  Creating necessary directories and files.  Please see working_on for my architecture ideas

---
+++ src/tcp/tcp_connection.c
@@ -0,0 +1,10 @@
+/* tcp_connection.c file
+	struct for handling each tcp_connection.  
+		Has socket_id that the kernal tcp_node can store to refer to.
+		maintains own statemachine
+		maintains own sliding window protocol
+*/
+
+
+		
+	
